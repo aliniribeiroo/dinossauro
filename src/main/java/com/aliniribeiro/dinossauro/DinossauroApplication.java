05defade29ba3b17b9997d63b1071af117933dd8
Alini Ribeiro <aliniribeiroo@gmail.com>
2018-07-08 13:54:10 -0300

    dinossauro

---
+++ src/main/java/com/aliniribeiro/dinossauro/DinossauroApplication.java
@@ -0,0 +1,12 @@
+package com.aliniribeiro.dinossauro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinossauroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinossauroApplication.class, args);
+	}
+}
